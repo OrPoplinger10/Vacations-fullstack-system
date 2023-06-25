df9625cee5ec9c53415951d90897564106b70cd6
OrPoplinger10 <orpoplinger777@gmail.com>
2023-06-25 13:43:16 +0300

    Handling the responsivity of the home titles and adding a background when shrinking the screen

--- frontend/src/Components/HomeArea/Home/Home.tsx
+++ frontend/src/Components/HomeArea/Home/Home.tsx
@@ -8,19 +8,19 @@ function Home(): JSX.Element {
     return (
         <div className="Home">
           <h1 className = "titleHome">
+            <div className="halfOfTitleHome">
           Create the best vacation for you  
-          <br/>
+          </div>
           <br/>
           and your family
           </h1>
-
           <p className="subTitleHome">
           Travel to your favorite city 
           <br/>
           with respectful of the environment!
          </p>
          <button className="bthHome">
-         <NavLink to="/contact" className= "homePage">Contact Us</NavLink>
+         <NavLink to="/contact"> Contact Us</NavLink>
          </button>
 
             
