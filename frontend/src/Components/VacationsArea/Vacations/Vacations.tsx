b40e40c2f6ed1e4a8446bbcec70c32080d737e8b
OrPoplinger10 <orpoplinger777@gmail.com>
2023-07-03 17:42:58 +0300

    Fixed a bug before filters that don't filter all the information now you can filter from all pages and not just from the first page

--- frontend/src/Components/VacationsArea/Vacations/Vacations.tsx
+++ frontend/src/Components/VacationsArea/Vacations/Vacations.tsx
@@ -9,82 +9,110 @@ import { vacationsStore } from "../../../Redux/VacationsState";
 import { Checkbox, FormControlLabel } from "@mui/material";
 import Filters from "../../../Models/Filters-model";
 
-
-
 function Vacations(): JSX.Element {
+  const [vacations, setVacations] = useState<VacationModel[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(8);
+  const [activeFilters, setActiveFilters] = useState<string[]>([]); // Changed the type to string[]
 
-    const [vacations, setVacations] = useState<VacationModel[]>([]);
-    const[currentPage, setCurrentPage] = useState(1);
-    const[postsPerPage] = useState(8);
-    const [activeFilters, setActiveFilters] = useState([]); 
+  // New state variable to hold the filtered vacations
+  const [filteredVacations, setFilteredVacations] = useState<VacationModel[]>([]);
 
-    useEffect(()=> {
-     vacationService.getAllVacations()
-     .then(dbVacations => setVacations(dbVacations))
-     .catch(err => notifyService.error(err));
-    }, [])
+  useEffect(() => {
+    vacationService
+      .getAllVacations()
+      .then((dbVacations) => setVacations(dbVacations))
+      .catch((err) => notifyService.error(err));
+  }, []);
 
-    const lastPostIndex = currentPage * postsPerPage;
-    const firstPostIndex = lastPostIndex - postsPerPage;
-    const currentPosts = vacations.slice(firstPostIndex, lastPostIndex);
+  // Function to handle filtering vacations based on active filters
+  function filterVacations() {
+    const vacationsData: VacationModel[] = vacationsStore.getState().vacations;
+    const now = new Date();
 
-    useEffect(()=>{
-        
-        const vacationsData:VacationModel[] = vacationsStore.getState().vacations;  
-        const now = new Date();
-        
-        // Filter object :
-        const filterOptions = {
-            isFollowing: (v: VacationModel) => v.isFollowing === 1,
-            actualVacations: (v: VacationModel) => new Date(v.startDate) > now,
-            startedVacations: (v: VacationModel) => (new Date(v.startDate) < now) && (new Date(v.endDate) > now),
-        };
-        
-       
-        const selectingFilters = (v: VacationModel) => {  
-            return activeFilters.every((filter) => filterOptions[filter as keyof typeof filterOptions](v));
-        };
-        
-        // Filter vacations
-        const filtered = vacationsData.filter(selectingFilters);
-        
-        setVacations(filtered);
-         
-    
-    },[activeFilters]);
+    // Filter object:
+    const filterOptions = {
+      isFollowing: (v: VacationModel) => v.isFollowing === 1,
+      actualVacations: (v: VacationModel) => new Date(v.startDate) > now,
+      startedVacations: (v: VacationModel) => new Date(v.startDate) < now && new Date(v.endDate) > now,
+    };
 
-    // Change on activeFilter will trigger useEffect with activeFilter dependency and run filter logic
-    function handleFilterChange(name: string, checked: boolean){
-        if(checked){
-            setActiveFilters(prevFilters => [...prevFilters, name]); // Add new filter to active filters 
+    const selectingFilters = (v: VacationModel) => {
+      return activeFilters.every((filter) => filterOptions[filter as keyof typeof filterOptions](v));
+    };
 
-        } else {
-            setActiveFilters(prevFilters => prevFilters.filter(f => f !== name)); // Remove unselected filter from active filters state
-        }
+    // Filter vacations
+    const filtered = vacationsData.filter(selectingFilters);
+
+    return filtered;
+  }
+
+  useEffect(() => {
+
+    // Update the useEffect hook to set the filteredVacations state using the filterVacations function and reset the current page to 1 when filters are changed.
+    setFilteredVacations(filterVacations());
+    setCurrentPage(1); // Reset the current page to 1 when filters are changed
+  }, [vacations, activeFilters]);
+
+  const lastPostIndex = currentPage * postsPerPage;
+  const firstPostIndex = lastPostIndex - postsPerPage;
+
+  // Display current posts from the filteredVacations array
+  const currentPosts = filteredVacations.slice(firstPostIndex, lastPostIndex);
+
+  // Change on activeFilter will trigger useEffect with activeFilter dependency and run filter logic
+  function handleFilterChange(name: string, checked: boolean) {
+    if (checked) {
+      setActiveFilters((prevFilters) => [...prevFilters, name]); // Add new filter to active filters
+    } else {
+      setActiveFilters((prevFilters) => prevFilters.filter((f) => f !== name)); // Remove unselected filter from active filters state
     }
-    
-    return (
-        <div className="Vacations">
-            <div className="mainTitle">
-            <h1 className="mainTitleUser">Our vacations</h1>
-            </div>
 
-            <div className="filterMenu">
-                <FormControlLabel control={<Checkbox />} onChange={(event) => handleFilterChange(Filters.IS_FOLLOWING, (event.target as HTMLInputElement).checked)} label="Favorites" />
-                <FormControlLabel control={<Checkbox />} onChange={(event) => handleFilterChange(Filters.ACTUAL_VACATIONS, (event.target as HTMLInputElement).checked)} label="Actual" />
-                <FormControlLabel control={<Checkbox />} onChange={(event) => handleFilterChange(Filters.STARTED_VACATIONS, (event.target as HTMLInputElement).checked)} label="Started" />
-            </div>
+    setCurrentPage(1); // Reset the current page to 1 when filters are changed
+  }
 
-        {currentPosts.map (v => <VacationCard key={v.vacationId} vacation={v}  /> )}  
+  return (
+    <div className="Vacations">
+      <div className="mainTitle">
+        <h1 className="mainTitleUser">Our vacations</h1>
+      </div>
 
-        <Pagination
-         totalPosts={vacations.length}
-         postsPerPage={postsPerPage}
-         setCurrentPage={setCurrentPage}
-         currentPage={currentPage}
+      <div className="filterMenu">
+        <FormControlLabel
+          control={<Checkbox />}
+          onChange={(event) =>
+            handleFilterChange(Filters.IS_FOLLOWING, (event.target as HTMLInputElement).checked)
+          }
+          label="Favorites"
         />
-        </div>
-    );
+        <FormControlLabel
+          control={<Checkbox />}
+          onChange={(event) =>
+            handleFilterChange(Filters.ACTUAL_VACATIONS, (event.target as HTMLInputElement).checked)
+          }
+          label="Actual"
+        />
+        <FormControlLabel
+          control={<Checkbox />}
+          onChange={(event) =>
+            handleFilterChange(Filters.STARTED_VACATIONS, (event.target as HTMLInputElement).checked)
+          }
+          label="Started"
+        />
+      </div>
+
+      {currentPosts.map((v) => (
+        <VacationCard key={v.vacationId} vacation={v} />
+      ))}
+
+      <Pagination
+        totalPosts={filteredVacations.length} // Use the length of filteredVacations
+        postsPerPage={postsPerPage}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+      />
+    </div>
+  );
 }
 
 export default Vacations;
