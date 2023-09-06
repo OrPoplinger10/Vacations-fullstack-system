3b140e3af7c7b92b00ba4e91baee3d1f29bd3abf
OrPoplinger10 <orpoplinger777@gmail.com>
2023-09-06 14:26:58 +0300

    Completion of uploading the site to the cloud and apprenticeship with AWS

--- frontend/src/Utils/AppConfig.ts
+++ frontend/src/Utils/AppConfig.ts
@@ -1,15 +1,20 @@
 
 class AppConfig{
-    
-    public registerUrl = "http://localhost:4000/api/auth/register/"
-    public loginUrl = "http://localhost:4000/api/auth/login/"
-    public vacationsUrl = "http://localhost:4000/api/vacations/"
-    public ordersUrl = "http://localhost:4000/api/orders/"
-    public contactsUrl = "http://localhost:4000/api/contacts/"
-    public followUrl = "http://localhost:4000/api/follow/"
-    public vacationImageUrl = "http://localhost:4000/api/vacations/images/"
-    public socketUrl = "http://localhost:4001"
- 
+
+    // public baseUrl ="http://localhost:4000" // dev
+    public baseUrl ="http://13.53.126.50:4000" // prod
+
+    // public socketUrl = "http://localhost:4001" // dev
+    public socketUrl = "http://13.53.126.50:4001" //prod
+        
+    public registerUrl =  this.baseUrl + "/api/auth/register/"
+    public loginUrl = this.baseUrl + "/api/auth/login/"
+    public vacationsUrl = this.baseUrl + "/api/vacations/"
+    public ordersUrl = this.baseUrl + "/api/orders/"
+    public contactsUrl = this.baseUrl + "/api/contacts/"
+    public followUrl = this.baseUrl + "/api/follow/"
+    public vacationImageUrl = this.baseUrl + "/vacations/images/"
+   
 }
 
 const appConfig = new AppConfig();
